0b41fa6a81c7b7321c795e5d305c459649ec5096
Navneesh Yadav <yadavnavneesh999@gmail.com>
2024-12-22 22:45:58 +0530

    just add process.env.MONGODB_URI

--- src/dbConfig/dbConfig.ts
+++ src/dbConfig/dbConfig.ts
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 export async function connect() {
     try {
-        mongoose.connect(process.env.MONGODB_URI);
+        mongoose.connect(process.env.MONGODB_URI!);
         const connection = mongoose.connection;
         connection.on('connection', () => {
             console.log("Mongodb connected successfully");
